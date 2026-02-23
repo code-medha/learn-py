f4e7d2713324c4f17326e7c16c07065618b97c96
devops-champ <dndarshan2@gmail.com>
2026-02-24 00:31:49 +0530

    post by id and delete

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, Response, status
+from fastapi import Depends, FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from . import models
@@ -29,7 +29,7 @@ def test_posts(db: Session = Depends(get_db)):
 def get_posts(db: Session = Depends(get_db)):
 
     virtual_mach = db.query(models.Vm).all()
-    return {"data": virtual_mach}
+    return {virtual_mach}
 
 @app.post("/vms", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session = Depends(get_db)):
@@ -38,17 +38,28 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
-
+    return {new_post}
 
 
 # {id} is called a path parameter
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    print(id)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post_id = db.query(models.Vm).filter(models.Vm.id == id).first()
+
+    if not post_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    return {post_id}
+
+@app.delete("/posts/{id}")
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post_del = db.query(models.Vm).filter(models.Vm.id == id)
+
+    if post_del.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+
+    post_del.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)   
 
 
-    post = find_post(id)
-    if not post:
-        response.status_code = status.HTTP_404_NOT_FOUND
-    return {"data": f"post {id}"}
